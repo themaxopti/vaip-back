07050a844e03c1499498934d880f2617ac451149
themaxopti <themaxopti@gmail.com>
2021-12-31 20:47:23 +0200

    asdasda

--- controllers/loginAndRegisterControllers/login.controller.js
+++ controllers/loginAndRegisterControllers/login.controller.js
@@ -70,7 +70,7 @@ exports.auth = async (req, res) => {
             return res.status(400).json({ message: 'Пользователь не найден' })
         }
 
-
+        console.log(req.cookies)
 
         res.json({
             token:req.cookies,
